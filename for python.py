a54d3ab9f2dfe9e9619775fe6a763c87486cdc36
Mirus <mega.yannov@gmail.com>
2026-07-20 14:03:35 +0300

    Update for python.py

--- for python.py
+++ for python.py
@@ -1,1 +1,12 @@
+import os
+import json
+from config import PROXY_CONFIG_PATH
 
+def _read_full_proxy_config():
+    if not os.path.exists(PROXY_CONFIG_PATH):
+        return None
+    try:
+        with open(PROXY_CONFIG_PATH, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except Exception:
+        return None
